a3a97cc14b4008d93a0292e2615dee7930f8fd60
StefRoo <49172522+StefRoo@users.noreply.github.com>
2019-04-02 00:44:00 +0200

    Update login_config.php

--- login_config.php
+++ login_config.php
@@ -1,10 +1,10 @@
 <?PHP
 require_once("./include/login_site.php");
 
-$fgmembersite = new FGMembersite();
+$loginsite = new LoginSite();
 
 //TO-DO: Link to database
 					  
-$fgmembersite->SetRandomKey('0Ga7GU0oMJ6NOxZ');
+$loginsite -> SetRandomKey('0Ga7GU0oMJ6NOxZ');
 
 ?>
